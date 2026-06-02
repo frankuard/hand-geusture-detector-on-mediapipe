3e481e2d5a3726574575ae014a61108a0709b846
Roshan Karki <roshankarki4956@gmail.com>
2026-06-02 23:05:25 +0545

    Importing required codes

---
+++ main.py
@@ -0,0 +1,6 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python.vision import HandLandmarker, HandLandmarkerOptions
+from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTaskRunningMode as VisionRunningMode
+
