22a7abb85f085512d6c53880f8216c0da020348a
Roshan Karki <roshankarki4956@gmail.com>
2026-06-03 10:59:25 +0545

    fINALIZING THE HAND TRACKING

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python.vision import HandLandmarker, HandLandmarkerOptions
@@ -31,6 +32,44 @@ while True:
     
     frame= cv2.flip(frame,1)
     
+    frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    
+    mp_image = mp.Image(image_format= mp.ImageFormat.SRGB,data=frame_rgb)
+    
+    timestamp_ms = int(time.time()* 1000)
+    
+    landmarker.detect_async(mp_image,timestamp_ms)
+    
+    result = latest_result[0]
+    
+    if result and result.hand_landmarks:
+        
+        hand_lms = result.hand_landmarks[0]
+        
+        h,w = frame.shape[:2]
+        
+        Connections = [(0,1),(1,2),(2,3),(3,4),(0,5),(5,6),(6,7),(7,8),(0,9),(9,10),(10,11),(11,12),(0,13),(13,14),(14,15),(15,16),(0,17),(17,18),(18,19),(19,20)]
+        
+        for a,b in Connections:
+            x1 = int(hand_lms[a].x*w)
+            y1 = int(hand_lms[a].y*h)
+    
+            x2 = int(hand_lms[b].x*w)
+            y2 = int(hand_lms[b].y*h)
+
+            cv2.line(frame,(x1,y1),(x2,y2),(255,255,255),2)
+        
+        landmarkers = list(range(21))
+        
+        for i in landmarkers:
+            lm = hand_lms[i]
+            x = int(lm.x*w)
+            y = int(lm.y*h)    
+            
+            cv2.circle(frame,(x,y),3,(0,200,0),-1)
+            
+            cv2.putText(frame,str(i),(x+5),(y-5),cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255),1)
+                    
     cv2.imshow("Hand Tracking", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
